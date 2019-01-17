50b540fa502749b7331adacf322172cb7f9f2c6a
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-17 07:48:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,7 @@ def create_project_hash
       image_link: project.css(".project-thumbnail img").attribute("src").value,
       description: project.css(".bbcard_blurb").text,
       location: project.css(".project-meta li a .location-name").text,
-      funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
     }
   end
