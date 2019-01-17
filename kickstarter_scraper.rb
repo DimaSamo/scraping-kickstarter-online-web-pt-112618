c0ec5f55bac7fce5702fd996bd0ad2af1bf4a6c9
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-17 07:45:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,11 +4,19 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  projects = {}
+  kickstarter.css(".project-card-wrap").each do |project|
+    projects[project] = {}
+  end
+  projects
+
+
   #kickstarter.css(".project-card-wrap") -- projects
   #project.css("h2.bbcard_name strong a").text -- title
   #project.css(".project-thumbnail img").attribute("src").value -- img URL
   #project.css(".bbcard_blurb").text -- Description
   #project.css(".project-meta li a .location-name").text -- Location
+  #project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i -- Percent Funded
   binding.pry
 end
 
